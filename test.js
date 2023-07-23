0529d1098e62645f0bd1270d0137d39b1c708b24
Vladimir Bolshakov <andersdeath@yandex.ru>
2023-07-23 22:39:28 +0400

    update

--- test.js
+++ test.js
@@ -1,6 +1,11 @@
 const { exec } = require("child_process");
-
+let i = 1;
 setInterval(() => {
+
+    console.log('==========');
+    console.log('Iteration ' + i++)
+    console.log('==========');
+
     exec("./commit.sh", (error, stdout, stderr) => {
         if (error) {
             console.log(`error: ${error.message}`);
