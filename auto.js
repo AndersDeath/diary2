3264f6194819bf392886a804d358eee6e46f66fb
Vladimir Bolshakov <andersdeath@yandex.ru>
2023-10-18 19:30:21 +0400

    update

--- auto.js
+++ auto.js
@@ -21,3 +21,16 @@ setInterval(() => {
     commit();
 }, 5000)
 
+process.on('exit', function(code) {
+    exec("./push.sh", (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+    });
+});
