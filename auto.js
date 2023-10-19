303b058c25a14fef80baddd9afb66d8fdc726ee4
Vladimir Bolshakov <andersdeath@yandex.ru>
2023-10-19 18:31:34 +0400

    update

--- auto.js
+++ auto.js
@@ -22,22 +22,22 @@ setInterval(() => {
 }, 5000)
 
 
-let callAmount = 0;
-process.on('SIGINT', function () {
-    if (callAmount < 1) {
-        exec("./push.sh", (error, stdout, stderr) => {
-            if (error) {
-                console.log(`error: ${error.message}`);
-                return;
-            }
-            if (stderr) {
-                console.log(`stderr: ${stderr}`);
-                return;
-            }
-            console.log(`stdout: ${stdout}`);
-        });
-        setTimeout(() => process.exit(0), 1000);
-    }
+// let callAmount = 0;
+// process.on('SIGINT', function () {
+//     if (callAmount < 1) {
+//         exec("./push.sh", (error, stdout, stderr) => {
+//             if (error) {
+//                 console.log(`error: ${error.message}`);
+//                 return;
+//             }
+//             if (stderr) {
+//                 console.log(`stderr: ${stderr}`);
+//                 return;
+//             }
+//             console.log(`stdout: ${stdout}`);
+//         });
+//         setTimeout(() => process.exit(0), 1000);
+//     }
 
-    callAmount++;
-});
+//     callAmount++;
+// });
