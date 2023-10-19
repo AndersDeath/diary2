41b54cf24841726509729c645f74b7220d7a5cfa
Vladimir Bolshakov <andersdeath@yandex.ru>
2023-10-19 18:30:18 +0400

    update

--- auto.js
+++ auto.js
@@ -25,7 +25,17 @@ setInterval(() => {
 let callAmount = 0;
 process.on('SIGINT', function () {
     if (callAmount < 1) {
-
+        exec("./push.sh", (error, stdout, stderr) => {
+            if (error) {
+                console.log(`error: ${error.message}`);
+                return;
+            }
+            if (stderr) {
+                console.log(`stderr: ${stderr}`);
+                return;
+            }
+            console.log(`stdout: ${stdout}`);
+        });
         setTimeout(() => process.exit(0), 1000);
     }
 
